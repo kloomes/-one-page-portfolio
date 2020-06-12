9d21b10a590d551f73c6ed3e115b59833438f9d4
Karl Loomes <kloomes@hotmail.com>
2020-06-12 13:39:30 +0100

    Routes and index page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'websites#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :websites, only: :index
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_06_12_123443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "websites", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "web_link"
+    t.string "github_link"
+    t.string "laptop_pic"
+    t.string "mobile_pic"
+    t.string "tags"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
