f947d4aa55eccfc7ee654e2354f7b6a7edc13846
Karl Loomes <kloomes@hotmail.com>
2020-06-12 14:06:09 +0100

    Seeds added

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,5 +1,5 @@
 class WebsitesController < ApplicationController
   def index
+    @websites = Website.all
   end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Website.create(
+  name: "Google",
+  description: "A great search engine.",
+  web_link: "google.com",
+  github_link: "www.github.com",
+  laptop_pic: "google_big.png",
+  mobile_pic: "google_small.png",
+  tags: "ruby, rails"
+  )
+
+Website.create(
+  name: "LinkedIn",
+  description: "Business platform meets social media and job search.",
+  web_link: "linkedin.com",
+  github_link: "www.github.com",
+  laptop_pic: "linkedin_big.png",
+  mobile_pic: "linkedin_small.png",
+  tags: "ruby, rails, react, javascript"
+  )
+
+Website.create(
+  name: "FitnessLab",
+  description: "Exercise tracking and analysis app",
+  web_link: "fitness-lab.fit",
+  github_link: "https://github.com/kloomes/rails-exercise-analysis-app",
+  laptop_pic: "fitlab_big.png",
+  mobile_pic: "fitlab_small.png",
+  tags: "ruby, rails"
+  )
